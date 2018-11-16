2f2c1af96e530cddf1f188a5b2f6628318aace37
oliviergenevest <olivier.genevest@gmail.com>
2018-11-16 19:16:40 +0100

    leaflet

---
+++ gatsby-node.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-leaflet/,
+            use: loaders.null(),
+             exclude: [
+        path.resolve(__dirname, '')
+      ],
+          },
+        ],
+      },
+    })
+  }
+}
+
